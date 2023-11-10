93333d7558edad6dfd44a8ff3e1b25d6d56e1a23
Kamalkumar <108823339+Patel-Kamalkumar@users.noreply.github.com>
2023-11-10 07:46:02 +0530

    Revert "Adding encrypted password transfer mechanism (#464)"
    This reverts commit bd18df98b8cef7b08bdb0df9b31ab22876d60683.

--- oem/ibm/libpldmresponder/file_io_by_type.cpp
+++ oem/ibm/libpldmresponder/file_io_by_type.cpp
@@ -5,7 +5,6 @@
 
 #include "common/utils.hpp"
 #include "file_io_type_cert.hpp"
-#include "file_io_type_chap.hpp"
 #include "file_io_type_dump.hpp"
 #include "file_io_type_lic.hpp"
 #include "file_io_type_lid.hpp"
@@ -188,10 +187,6 @@ std::unique_ptr<FileHandler> getHandlerByType(uint16_t fileType,
         case PLDM_FILE_TYPE_PSPD_VPD_PDD_KEYWORD:
         {
             return std::make_unique<keywordHandler>(fileHandle, fileType);
-        }
-        case PLDM_FILE_TYPE_CHAP_DATA:
-        {
-            return std::make_unique<ChapHandler>(fileHandle, fileType);
         }
         default:
         {
